4672f36e0029344da8e6bb82b58dcc14de2fbcaf
dbcabanes <dborrascabanes@hotmail.com>
2019-10-02 13:48:43 +0200

    añadimos el app3.js que estaba en blanco

---
+++ app3.js
@@ -0,0 +1,9 @@
+//Crear una interfaz es como crear una estructura y datos obligatorios 
+function enviarMision(xmen) {
+    console.log("Eniviando a " + xmen.nombre);
+}
+var wolverine = {
+    nombre: "Wolverine",
+    poder: "Regeneracion"
+};
+enviarMision(wolverine);
